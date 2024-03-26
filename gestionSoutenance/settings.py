f9260ea61f774425f37afeb39b5299dd216952bf
MERIEM <meriembenaissa066@gmail.com>
2024-03-26 22:56:32 +0100

    commit modals

--- gestionSoutenance/settings.py
+++ gestionSoutenance/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'livereload.middleware.LiveReloadScript',
+
 ]
 
 ROOT_URLCONF = 'gestionSoutenance.urls'
