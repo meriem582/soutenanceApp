5e5f53413a2cf2843112addf2d29a3bdf42d555a
MERIEM <meriembenaissa066@gmail.com>
2024-03-31 02:04:50 +0100

    Commit (BOUTON+ENSEIGNANT(OCCUPATION,DOMAINE A CONTINUER)

--- gestionSoutenance/settings.py
+++ gestionSoutenance/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'livereload',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -46,7 +45,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'livereload.middleware.LiveReloadScript',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
