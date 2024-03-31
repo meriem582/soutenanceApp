b5d6431188924b145ebd8e279bc08ee0fd1c45c7
MERIEM <meriembenaissa066@gmail.com>
2024-03-31 02:11:41 +0100

    Commit Meriem Enseignant(OCCUPATION+DOMAINE A CONTINUER)

--- soutenanceApp/models.py
+++ soutenanceApp/models.py
@@ -8,12 +8,8 @@ class Utilisateur(models.Model):
     nom=models.CharField(max_length=50)
     prenom=models.CharField(max_length=50)
     
-
-
 class Meta:
     db_table="utilisateur"
-
-
 
 class Administrateur(models.Model):
     email=models.ForeignKey(Utilisateur,on_delete=models.CASCADE,primary_key=True)
@@ -21,8 +17,6 @@ class Administrateur(models.Model):
 
 class Meta:
     db_table="administrateur"
-
-
 
 class Salle(models.Model):
     num_bloc=models.IntegerField()
@@ -35,7 +29,6 @@ class Meta:
 class Meta:
     db_table="salle"
 
-
 class Occupation_salle(models.Model):
     ids = models.AutoField(primary_key=True)
     date_occupation=models.DateField()
@@ -47,15 +40,12 @@ class Occupation_salle(models.Model):
 class Meta:
     db_table="occupation_salle"
 
-
-
 class Enseignant(models.Model):
     email=models.ForeignKey(Utilisateur,on_delete=models.CASCADE,primary_key=True)
     grade=models.CharField(max_length=50)
 
 class Meta:
     db_table="enseignant"
-
 
 class Occupation_Enseignant(models.Model):
     ide = models.AutoField(primary_key=True)
@@ -67,23 +57,19 @@ class Occupation_Enseignant(models.Model):
 class Meta:
     db_table="occupation_enseignant"
 
-
-
 class Domain_expertise(models.Model):
     intitule=models.CharField(max_length=50)
     idAdministrateur=models.ForeignKey(Administrateur,on_delete=models.CASCADE)
+
+class Meta:
+    db_table="domaine_expertise"
 
 class EnseignantDomaineExpertise(models.Model):
     idEnseignant=models.ForeignKey(Enseignant,on_delete=models.CASCADE)
     idDomaineExpertise=models.ForeignKey(Domain_expertise,on_delete=models.CASCADE)
     
-
-
 class Meta:
-    db_table="domaine_expertise"
-     
-
-
+    db_table="EnseignantDomaineExpertise"
 
 class Theme(models.Model):
     intitule=models.CharField(max_length=50,unique=True)
@@ -106,11 +92,8 @@ class Leader(models.Model):
     idEnseignantEncadrant=models.ForeignKey(Enseignant,on_delete=models.CASCADE)
     idTheme=models.OneToOneField(Theme,on_delete=models.CASCADE)
 
-
 class Meta:
     db_table="leader"
-
-
 
 class Demande(models.Model):
     theme=models.CharField(max_length=50)
@@ -131,15 +114,5 @@ class Evaluation(models.Model):
     idEnseignant=models.ForeignKey(Enseignant,on_delete=models.CASCADE)
     idSalle=models.ForeignKey(Salle,on_delete=models.CASCADE)
 
-
-
-
 class Meta:
     db_table="evaluation"
-
-
-
-
-
-
-    
