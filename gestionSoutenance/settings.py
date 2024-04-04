d05bebbcc0aaed5432b5388556f95a5b4e76f0d5
Coscreen <bouchouchasamyelias@gmail.com>
2024-04-04 06:17:43 +0100

    Design des tableaux en cours (tests seulement)

--- gestionSoutenance/settings.py
+++ gestionSoutenance/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'soutenanceApp.apps.SoutenanceappConfig',
+    'livereload',
+
 
 ]
 
@@ -49,7 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'livereload.middleware.LiveReloadScript',
+    
 ]
 
 ROOT_URLCONF = 'gestionSoutenance.urls'
