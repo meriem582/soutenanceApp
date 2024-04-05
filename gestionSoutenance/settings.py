88995b644f0ff0e3ad542a14f85659259480e60d
Coscreen <bouchouchasamyelias@gmail.com>
2024-04-05 04:50:36 +0100

    Bug de la navBar fixé + design

--- gestionSoutenance/settings.py
+++ gestionSoutenance/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'soutenanceApp.apps.SoutenanceappConfig',
+    'livereload',
+
 
 
 ]
@@ -50,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
+
     
 ]
 
