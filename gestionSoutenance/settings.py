c4988a420fee99ab4510c45c3a5678f97e291033
Coscreen <bouchouchasamyelias@gmail.com>
2024-04-12 23:21:28 +0100

    ...

--- gestionSoutenance/settings.py
+++ gestionSoutenance/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'soutenanceApp.apps.SoutenanceappConfig',
-    'livereload',
+    # 'livereload',
 
 
 
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
+    # 'livereload.middleware.LiveReloadScript',
 
     
 ]
